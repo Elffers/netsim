34e546a52568b3c54b4b09ad227557909d513152
Hsing-Hui Hsu <hsing-hui@carezone.com>
2016-01-30 15:16:50 -0800

    Add TODO for IPv4Interface

--- ipv4_interface.rb
+++ ipv4_interface.rb
@@ -1,3 +1,9 @@
+# TODO: An ethernet interface is a hardware interface, modeled by the
+# Interface class. An IPv4 interface is a software interace that
+# allows IPv4 packets to be encapsulated in ethernet packets.
+# Therefore it is not really a subclass of an ethernet interface, but
+# rather belongs to one.
+
 class IPv4Interface < Interface
   attr_accessor :ip_address
   attr_accessor :subnet_mask_size
