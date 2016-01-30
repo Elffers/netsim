17252405c8fc97ee5463af00dc5ff898ac8fedba
Hsing-Hui Hsu <hsing-hui@carezone.com>
2016-01-30 13:56:29 -0800

    Refactor MacAddress.next to initialize @next_mac

--- mac_address.rb
+++ mac_address.rb
@@ -2,11 +2,12 @@
 # a network (actually, it's globally unique).  Conversely, a network is
 # a collection of hosts that can communicate with each other using their
 # interfaces' MAC addresses for addressing.
-#
+
 class MacAddress
   attr_reader :bytes
 
   @mutex = Mutex.new
+  @next_mac = 0
 
   def initialize(bytes)
     raise "6 bytes please" unless bytes.length == 6
@@ -27,12 +28,12 @@ class MacAddress
   end
 
   # The all-ones address is the broadcast address (by definition)
-  #
   BROADCAST = new("\xff\xff\xff\xff\xff\xff")
 
   def self.next
     @mutex.synchronize do
-      @next_mac = @next_mac ? @next_mac + 1 : 1
+      @next_mac += 1
+      # packs the mac_address into hex format
       new([@next_mac].pack("xxl>"))
     end
   end
